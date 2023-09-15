3a67ed2afd7e1c987a554a61c3ea5dcd632eb065
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-09-15 19:51:17 -0300

    ✨ Uso do reteamento ✨

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,16 +1,16 @@
+import { Routes } from './Routes'
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
 import './styles/global.css'
 
 export function App() {
   return (
-    <div className="h-screen w-screen  bg-rotion-400 text-rotion-100">
+    <div className="h-screen w-screen bg-rotion-400 text-rotion-100">
       <Sidebar />
       <div className="flex-1 flex flex-col max-h-screen">
         <Header />
-        <main className="flex-1 flex items-center justify-center text-rotion-400">
-          Selecione ou crie um documento
-        </main>
+
+        <Routes />
       </div>
     </div>
   )
